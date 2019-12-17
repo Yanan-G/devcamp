6f9937c790be3e5810bc24603143c8b2c5c4826f
yanan <geng.yan@husky.neu.edu>
2019-12-17 13:15:39 -0500

    refactor

---
+++ controllers/bootcamps.js
@@ -0,0 +1,19 @@
+exports.getBootcamps = (req, res, next) => {
+	res.status(200).json({})
+}
+
+exports.getBootcamp = (req, res, next) => {
+	res.status(200).json({})
+}
+
+exports.createBootcamp = (req, res, next) => {
+	res.status(200).json({})
+}
+
+exports.updateBootcamp = (req, res, next) => {
+	res.status(200).json({})
+}
+
+exports.deleteBootcamp = (req, res, next) => {
+	res.status(200).json({})
+}
